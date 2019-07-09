6e32e97bea76b9b9ebef3e8ecd15498177f0d8b1
prabaharanh <prabaharan.haridass@outlook.com>
2019-07-09 22:10:21 +1200

    title change -- ph

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>EmpInfo</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -84,7 +84,7 @@
                     EmpInfo
                 </div>
 
-                
+
             </div>
         </div>
     </body>
